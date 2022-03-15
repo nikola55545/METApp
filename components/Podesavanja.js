c62d3fb2ba33ecdd7f2ec0125ad2bb7a5ada7d88
Nikola Knežević <nikolak2504@gmail.com>
2022-03-15 21:58:47 +0100

    Podesavanja dizajn

--- components/Podesavanja.js
+++ components/Podesavanja.js
@@ -6,34 +6,120 @@ import {
   Button,
   TouchableOpacity,
   Dimensions,
+  Image,
+  Switch
 } from "react-native";
 import React, { Component, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import ShareIcon from '../assets/icons/info.png';
+import InfoIcon from '../assets/icons/share.png';
+
 export default class Podesavanja extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeSwitch: null,
+    }
+  }
+  toggleSwitch = (switchNumber) => {
+    this.setState({
+      activeSwitch: switchNumber === this.state.activeSwitch ? null : switchNumber
+    })
+  };
+  switchOne = (value) => { this.toggleSwitch(1) };
+  switchTwo = (value) => { this.toggleSwitch(2) };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "flex-start", paddingTop: 10 }}>
         <StatusBar style="dark" />
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('OAplikaciji')}>
-          <Text style={{marginBottom: 50}}>O Aplikaciji</Text>
+
+        <View style={styles.button}>
+          <Switch
+            trackColor={{ false: "#767577", true: "#c9093d" }}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={this.switchOne}
+            value={this.state.activeSwitch === 1}
+            style={styles.switch}
+          />
+          <View>
+            <Text>Zimbra Mail</Text>
+            <Text style={{ color: 'grey' }}>Obaveštenja o novim porukama</Text>
+          </View>
+        </View>
+
+        <View style={styles.button}>
+          <Switch
+            trackColor={{ false: "#767577", true: "#c9093d" }}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={this.switchTwo}
+            value={this.state.activeSwitch === 2}
+            style={styles.switch}
+          />
+          <View>
+            <Text>Notifikacije</Text>
+            <Text style={{ color: 'grey' }}>Važne informacije - obaveštenja</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('OAplikaciji')}>
+          <Image source={InfoIcon} style={styles.buttonIcon} />
+          <View>
+            <Text>O MET Aplikaciji</Text>
+            <Text style={{ color: 'grey' }}>Od MET studenata, za MET studente</Text>
+          </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.logoutButton} onPress={() => this.props.navigation.reset({ index: 0, routes: [{name: 'Login'}] })}>
-          <Text style={{color: 'white'}}>ODJAVI SE</Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => alert('Share Context')}>
+          <Image source={ShareIcon} style={styles.buttonIcon} />
+          <View>
+            <Text>Preporuči prijatelju</Text>
+            <Text style={{ color: 'grey' }}>Preporuči MET aplikaciju svom prijatelju</Text>
+          </View>
         </TouchableOpacity>
+
+        <TouchableOpacity style={styles.logoutButton} onPress={() => this.props.navigation.reset({ index: 0, routes: [{ name: 'Login' }] })}>
+          <Text style={{ color: '#c9093d' }}>ODJAVA</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
-  logoutButton:{
-    backgroundColor: '#c9093d',
+  logoutButton: {
+    backgroundColor: 'white',
     borderRadius: 100,
     paddingTop: 15,
     paddingBottom: 15,
-    paddingStart: 20,
-    paddingEnd: 20,
+    paddingStart: 45,
+    paddingEnd: 45,
+    marginTop: 7.5,
+  },
+  button: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: '90%',
+    borderRadius: 10,
+    marginTop: 7.5,
+    marginBottom: 7.5,
+  },
+  buttonIcon: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    marginEnd: 15,
+  },
+  switch: {
+    transform: [{ scaleX: .9 }, { scaleY: .9 }],
+    marginStart: -10,
+    marginEnd: 15,
   }
 });
